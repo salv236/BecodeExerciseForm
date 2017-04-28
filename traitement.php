d9fbd5a0ede2ed0a473698aec35db80970b55823
LudovicPatho <ludovic.patho@gmail.com>
2017-04-28 09:44:55 +0200

    Ajout ludo

--- traitement.php
+++ traitement.php
@@ -1,31 +1,90 @@
-<? 
 
-/* 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UFT-8">
+		<link rel="stylesheet" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<title>Technical support Hackers poulette</title>
+	</head>
+	<body>
+  		<div class="container">
 
-1. create a variable called validate with $POST submit 
-2. create a variable for  $name each required field  $post ["name"]
-3. create a variable $lastname for each required field  $post ["lastName"]
-4. create a variable $mail for each required field  $post ["mail"]
-5.create a variable $country for each required field  $post ["country"]
-6.create a variable $tel for each required field  $post ["country"]
+	    <header class="text-center"><a href="#"> <img class="pull-left" src="assets/img/logo.png" alt="Logo Hackers poulette"/></a>
+			
+			<div class="main-header">
+	    		<h1>Technical support Hackers poulette</h1>
+	     		<p>How can we improve your experience</p>
+	     	</div>
 
-*/
+	    </header>
 
-$validate = $_POST['submit'];
-$name = $_POST['name'];
-$lastname = $_POST['lastName'];
-$mail = $_POST['mail'];
-$country = $_POST['country'];
-$tel = $_POST['tel'];
+	<main class="form-container">
 
-echo "you name is " .$name ."<br>";
-echo "you lastname is " .$lastname ."<br>";
-echo "you email is " .$mail ."<br>";
-echo "you country is " .$country ."<br>";
-echo "you tel no: is " .$tel. "<br>";
+		<!-- Ici commence le php -->
 
-print_r($name);
+		<?php
+		
+		if (isset($_POST['mail'])) 
+		{ 
+			$_POST['mail'] = htmlspecialchars($_POST['mail']); // On rend inoffensives les balises HTML que le visiteur a pu rentrer
+
+									  // { Minium caractère, Maximum caractère }	
+    			    	if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) 
+				{
+       				 $mail = $_POST['mail'];
+				}
+
+    				else
+   				{
+
+        			$mail = "Your adress mail is incorrect";
+				}
+
+		}
+
+	
+		$validate = $_POST['submit'];
+		$name = htmlspecialchars($_POST['name']);
+		$lastname = htmlspecialchars($_POST['lastName']);
+		$country = htmlspecialchars($_POST['country']);
+		$tel = htmlspecialchars($_POST['tel']);
+		$productId = htmlspecialchars($_POST["productId"]);
+		$customerNumber = htmlspecialchars($_POST["customerNumber"]);
+		$gender = $_POST["gender"];
+		$date = htmlspecialchars($_POST["date"]);
+		$message = htmlspecialchars($_POST["message"]);
+		$subject = $_POST["subject"];
+
+		$additionNumber = 2;
+		$additionNumber2 = 14;
+		$totaladdition = $additionNumber + $additionNumber2;
 
 
+		print_r(" Hello " . $name . "<br />");
+		echo "you name is " . $name ."<br>";
+		echo "you lastname is " . $lastname ."<br>";
+		echo "mail : " . $mail ."<br>";
+		echo "you country is " . $country ."<br>";
+		echo "you tel no: is " . $tel . "<br>";
+		echo "Product ID :" . $productId . "<br >";
+		echo "Customer Number :" . $customerNumber . "<br >";
+		echo "Date :" . $date . "<br />";
+		echo "Subject :" . $subject ."<br />"; 
+		echo "Message :" . $message . " <br />";
+		echo "Your gender:" . $gender . "<br />";
 
-?>
+	
+		echo  "Your number of complaints " . $totaladdition . " <br />";	
+
+
+		?> 
+		
+		<!-- Ici se termine le php -->
+	</main>
+
+    		<script src="assets/js/jquery-3.2.1.min.js"></script>
+   	       <script src="assets/js/bootstrap.js"></script>
+  		</div> <!--container-->
+</body>
+</html>
